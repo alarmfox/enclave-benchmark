5e14cd7d04b11df7c859f0b7e106feb75c1de404
giuseppe <capassog97@gmail.com>
2025-02-22 15:14:24 +0100

    Dropped nbody support since not applicable with Gramine; using sysbench

--- examples/nbody/.github/install-post.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# This is a post-install script common to all Docker images.
-#
-set -x
-meson --version
-g++ --version
-clang++ --version
-exit 0
--- examples/nbody/.github/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-. .github/install-${IMAGE//\//-}.sh
